a480b0904aa8edb9a1e6c26347b935ec9ce9cf8f
cristiansot <soluciongrafix@gmail.com>
2024-11-16 13:59:37 -0300

    Se imprimen de manera dinámica las cards en el front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
+/* Ruta de aprendizaje 
 //https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
+//https://www.youtube.com/watch?v=WTbni8QMos0&ab_channel=CodingTube
+*/
 
 let equipoData;
+let cardsContainer = document.querySelector('.cards__container');
 
 fetch('./equipo.json')
   .then(function (response) {
@@ -14,11 +18,24 @@ fetch('./equipo.json')
   .then(function (data) {
     equipoData = data;
     console.log(equipoData);
+
+    equipoData.forEach(element => {
+      cardsContainer.innerHTML += `
+        <div class="card mb-5">
+          <div class="card-body">
+            <h4 class="card-title">${element.nombre}</h4>
+            <h5 class="card-title">${element.especialidad}</h5>
+            <p class="card-text">${element.resumen}</p>
+          </div>
+        </div>`
+      console.log(element);
+    });
+    let cards = document.querySelectorAll('.card');
+    console.log(cards)
   })
   .catch(function (error) {
     console.log("Hubo un problema con la petición Fetch:" + error.message);
   });
 
-equipoData.forEach(element => {
-  console.log(element);
-});
+
+
