f59f9d4a9ff80dbb82a9612c953293d2f7523077
cristiansot <soluciongrafix@gmail.com>
2024-11-17 14:33:02 -0300

    Prompts reciben mensajes, se imprimen en pantalla y se muestran en consola

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,10 +1,30 @@
 const userNameInput = document.getElementById('userName');
+const emailInput = document.getElementById('email');
+const asuntoInput = document.getElementById('asunto');
+const mensajeInput = document.getElementById('mensaje')
 
 userNameInput.addEventListener('click', () => {
-  const userName = prompt('Por favor, ingresa tu nombre:');
-  if (userName) {
-    userNameInput.value = userName; 
-    console.log(userName);
-  }
+  const userName = prompt('Ingresa tu nombre:');
+  userNameInput.value = userName; 
+  console.log(userName);
 });
+
+emailInput.addEventListener('click', () => {
+    const email = prompt('Ingresa tu correo electrónico');
+    emailInput.value = email;
+    console.log(email);
+})
+
+asuntoInput.addEventListener('click', () => {
+    const asunto = prompt('Ingrese el asunto');
+    asuntoInput.value = asunto;
+    console.log(asunto);
+})
+
+mensajeInput.addEventListener('click', () => {
+    const mensaje = prompt('Ingrese el mensaje');
+    mensajeInput.value = mensaje;
+    console.log(mensaje);
+})
+
 
